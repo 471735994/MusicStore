66ed594b5dc2cd9b14f5525ea92da81e1f0bea44
471735994 <471735994@qq.com>
2026-07-16 01:31:58 +0800

    初始化

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace MusicStore.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MusicStore.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
