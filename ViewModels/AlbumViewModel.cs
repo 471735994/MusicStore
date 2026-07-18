3565f064f4e5563159e80680ac5c80f4ea22177c
471735994 <471735994@qq.com>
2026-07-19 01:41:05 +0800

    初步完成了专辑搜索

--- ViewModels/AlbumViewModel.cs
+++ ViewModels/AlbumViewModel.cs
@@ -1,5 +1,21 @@
+using System.Threading.Tasks;
+using Avalonia.Media.Imaging;
+using MusicStore.Models;
+
 namespace MusicStore.ViewModels;
 
 public class AlbumViewModel : ViewModelBase
 {
+    private readonly Album _album;
+    public AlbumViewModel(Album album)
+    {
+        _album = album;
+    }
+    public string Artist => _album.Artist;
+    public string Title => _album.Title;
+    public Task<Bitmap?> Cover => LoadCoverAsync();
+    private async Task<Bitmap?> LoadCoverAsync()
+    {
+        return null;
+    }
 }
--- ViewModels/MusicStoreViewModel.cs
+++ ViewModels/MusicStoreViewModel.cs
@@ -1,5 +1,7 @@
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
+using MusicStore.Services;
 
 namespace MusicStore.ViewModels;
 
@@ -9,13 +11,25 @@ public partial class MusicStoreViewModel : ViewModelBase
     [ObservableProperty] public partial bool IsBusy { get; set; }
     [ObservableProperty] public partial AlbumViewModel? SelectedAlbum { get; set; }
     public ObservableCollection<AlbumViewModel> SearchResults { get; } = new();
-
-    // 模拟数据
-    public MusicStoreViewModel()
+    private static readonly AlbumService s_albumService = new();
+    partial void OnSearchTextChanged(string? oldValue, string? newValue)
     {
-        SearchResults.Add(new AlbumViewModel());
-        SearchResults.Add(new AlbumViewModel());
-        SearchResults.Add(new AlbumViewModel());
+        _ = DoSearch(SearchText);
     }
+    private async Task DoSearch(string? term)
+    {
+        IsBusy = true;
+        SearchResults.Clear();
+
+        var albums = await s_albumService.SearchAsync(term);
+        foreach (var album in albums)
+        {
+            var vm = new AlbumViewModel(album);
+            SearchResults.Add(vm);
+        }
+        IsBusy = false;
+    }
+
+
 
 }
